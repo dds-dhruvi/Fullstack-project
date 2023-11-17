c299feb1553a983d8fcf20fd2fc9969630b2315a
dds-dhruvi <dhruvids@gmail.com>
2023-11-17 20:06:27 +0530

    update dependencies

--- src/main/java/com/practice/fullstack/FullstackApplication.java
+++ src/main/java/com/practice/fullstack/FullstackApplication.java
@@ -1,9 +1,13 @@
 package com.practice.fullstack;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+
 public class FullstackApplication {
 
 	public static void main(String[] args) {
